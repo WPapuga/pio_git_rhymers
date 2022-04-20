14c2b0a22c41a5d21e9ac9ffcc9460a9c506b24a
WPapuga <79088499+WPapuga@users.noreply.github.com>
2022-04-20 19:13:54 +0200

    9. Wygnerowanie gettera i settera dla zmiennej total w klasie DefaultCountingOutRhymers

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,6 +7,15 @@ public class DefaultCountingOutRhymer {
 	private static final int FULL = 11;
 
 	private final int[] numbers = new int[CAPACITY];
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	private int total = -1;
 
 
